d2ffdb7d91de37dda422ea6bde727d89683cb032
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2014-07-17 11:54:28 +0200

    Please refactor me

--- HelloWorld/Controllers/HomeController.cs
+++ HelloWorld/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HelloWorld.Models;
 
 namespace HelloWorld.Controllers
 {
@@ -10,7 +11,7 @@ namespace HelloWorld.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View(ToDoListModel.GetAllToDos());
         }
     }
 }
---
+++ HelloWorld/Models/ToDoListModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloWorld.Models
+{
+    public class ToDoListModel
+    {
+        public static IEnumerable GetAllToDos()
+        {
+            return new object[]
+                {
+                    new object[] { 1, "ToDo #1", "Descr", true, new DateTime(2014, 7, 17) },
+                    new object[] { 2, "ToDo #2", "Descr", false, new DateTime(2014, 7, 18) },
+                    new object[] { 3, "ToDo #3", "Descr", false, new DateTime(2014, 7, 19) },
+                    new object[] { 4, "ToDo #4", "Descr", false, new DateTime(2014, 7, 20) },
+                    new object[] { 5, "ToDo #5", "Descr", false, new DateTime(2014, 7, 21) },
+                };
+        }
+    }
+}
