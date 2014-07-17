a0f3c6f49d5bce40b4fdfe3ad283405c3af0c392
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2014-07-17 16:14:15 +0200

    javasscript bundle

---
+++ HelloWorld/Scripts/superGrid.js
@@ -0,0 +1,13 @@
+﻿var superGrid = (function() {
+
+    var priv = "Test";
+
+    var onselectionChange = function() {
+        
+
+    }
+
+    return {
+        onselectionChange: onselectionChange
+    }
+})();
